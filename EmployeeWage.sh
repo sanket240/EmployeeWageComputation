3b165110bd2fdc33892cf85602fd979d5e821f51
sanket2498 <sanketdulange24@gmail.com>
2020-10-31 18:05:34 +0530

    Added : Employee Attendance

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,1 +1,9 @@
 echo "Employee Wage Compuation Problem"
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+if [[ $isPresent -eq $randomCheck ]]
+then
+    echo "Employee is present"
+else
+     echo "Employee is not present"
+fi
