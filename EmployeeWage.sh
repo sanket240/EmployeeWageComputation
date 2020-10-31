d38cda3926ef739c263d326d9720bd7dcd964c5d
sanket2498 <sanketdulange24@gmail.com>
2020-10-31 19:00:22 +0530

    Added : Conditional Wages

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,6 +2,8 @@ echo "Employee Wage Compuation Problem"
 isPresent=1
 wageRate=20
 dayHr=8
+days=0
+employeeWorkingHours=0
 partTimeHr=$(($RANDOM%8+1))
 workingDaysPerMonth=20
 echo "1.Employee Attendance 2.Daily Wage 3.Part Time Wage"
@@ -28,3 +30,20 @@ esac
 dailyWage=$(( $wageRate * $dayHr ))
 monthlyWage=$(( $dailyWage * $workingDaysPerMonth ))
 echo "Monthly Wage=$monthlyWage"
+echo "Wages " 
+while [[ $days -lt 20 && $employeeWorkingHours ]]
+do
+	        employeeCheck=$(($RANDOM%2+1))
+        case $employeeCheck in
+        1)
+                employeeWorkingHours=$(( $empHr + 8 ))
+        ;;
+	2)
+                employeeWorkingHours=$(( $empHr + 4 ))
+        ;;
+        esac
+	salary=$(( $employeeWorkingHours * $wageRate ))
+	((days++))
+	echo "$salary "
+
+done
